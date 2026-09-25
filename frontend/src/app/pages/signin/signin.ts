e443d70e5fb0d95432fd709cda1976895df92623
Gustavooxx <gustaviixx@gmail.com>
2026-09-25 11:54:23 -0300

    Adicionada a página de login e sign in

---
+++ frontend/src/app/pages/signin/signin.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-signin',
+  styleUrl: './signin.css',
+  templateUrl: './signin.html',
+})
+export class Signin {}
