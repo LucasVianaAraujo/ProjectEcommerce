c716496be838c0334837db0d8ce4cfa753653c2b
Lucas Viana <luks79180@gmail.com>
2026-09-25 15:03:11 -0300

    Validação adicionada na tela de login

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Footer } from './shared/footer/footer';
+import { Login } from './pages/login/login';
 
 @Component({
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Login],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
@@ -12,5 +13,3 @@ import { Footer } from './shared/footer/footer';
 export class App {
   protected readonly title = signal('OldShoes');
 }
-
-
--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [FormsModule, RouterLink],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
-export class Login {}
+export class Login {
+
+  email = '';
+  senha = '';
+
+  enviar() {
+    if (this.email == 'admin@gmail.com' && this.senha == '1234') {
+      alert('Credenciais Corretas!')
+    } else {
+      alert('Credenciais inválidas!');
+    }
+  }
+}
